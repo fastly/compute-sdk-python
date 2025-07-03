52c5616f3644ab53c4713489b4b06a5a729bfac6
Erik Rose <erik.rose@fastly.com>
2025-07-03 16:51:12 -0400

    Add an adapter to serve WSGI requests through an actual Python web framework.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+from io import StringIO
+from urllib.parse import urlparse
+
+from bottle import route, run, template, Bottle
+
 from wit_world.exports import Reactor as BaseReactor
 from wit_world.imports import log, http_req, http_resp, http_body
 from wit_world.imports.http_body import read, write
 from wit_world.imports.http_resp import send_downstream
-import sys
-import traceback
-import io
-import art
+
+
+app = Bottle()
+
+
+@app.route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
 
 
 def init():
@@ -17,22 +26,55 @@ def print(msg):
     log.write(log_ep, msg.encode())
 
 
-def do_serve(req: int, body: int) -> None:
-    # resp.set_status(418)
-    request_body = read(body, 1024)
-    print(f"Look at your body: {request_body}")
+# def do_serve(req: int, body: int, app: Bottle) -> None:
+#     
+# 
+# 
+#     request_body = read(body, 1024)
+#     print(f"Look at your body: {request_body}")
+# 
+#     response_body = http_body.new()
+#     write(response_body, b"bytes!", http_body.WriteEnd.BACK)
+#     
+#     response = http_resp.new()
+#     http_resp.header_append(response, b"Foof", b"Barf")
+#     send_downstream(response, response_body, True)
 
-    response_body = http_body.new()
-    write(response_body, b"bytes!", http_body.WriteEnd.BACK)
+
+def serve_wsgi_request(req, body, app):
+    """Pass a WSGI application a single request, and adapt what it hands us back
+    to the Fastly API."""
     
     response = http_resp.new()
+
+    def start_response(status: str, headers: list[tuple], exc_info=None):
+        code, _description = status.split(" ", 1)
+        http_resp.status_set(response, int(code))
+        for header, value in headers:
+            print(f"Header: {header}. Value: {value}.")
+            http_resp.header_append(response, header.encode(), value.encode())
+        return lambda x: None  # TODO: Return a real write().
+
+    response_body = http_body.new()  # TODO: Maybe stream.
+    url = urlparse(http_req.uri_get(req, 2048))
+    environ = {"REQUEST_METHOD": http_req.method_get(req, 12),
+               "PATH_INFO": url.path,
+               "QUERY_STRING": url.query,
+               "SERVER_NAME": url.hostname,
+               "SERVER_PORT": url.port,
+               "wsgi.errors": StringIO()  # TODO: Replace with a file-like wrapper around the log endpoint.
+    }
+    print(f"{environ}")
+    for body_chunk in app(environ, start_response):
+        write(response_body, body_chunk, http_body.WriteEnd.BACK)
     send_downstream(response, response_body, False)
+
 
 class Reactor(BaseReactor):
     def serve(self, req: int, body: int) -> None:
         init()
         try:
-            do_serve(req, body)
+            serve_wsgi_request(req, body, app)
         except Exception as e:
             # outf = Stdout()
             print(f"Exception {e}")
