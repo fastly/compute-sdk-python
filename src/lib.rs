c875f6ab8bd224a186b8c580f729b2f3eb99d811
Erik Rose <erik.rose@fastly.com>
2025-09-26 14:47:07 -0400

    Rename the whole shooting match to "Wasiless".

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 wit_bindgen::generate!({
-    world: "python-virt",
+    world: "wasiless",
     path: "wit",
     generate_all,
 });
@@ -32,9 +32,11 @@ impl GuestTerminalInput for TerminalInput {
     }
 }
 
-struct MyComponent;
+/// Wasm component implementing WASI with as little functionality as possible
+/// without trapping
+struct Wasiless;
 
-impl terminal_input::Guest for MyComponent {
+impl terminal_input::Guest for Wasiless {
     type TerminalInput = TerminalInput;
 }
 
@@ -55,14 +57,14 @@ impl GuestTerminalOutput for TerminalOutput {
     }
 }
 
-impl terminal_output::Guest for MyComponent {
+impl terminal_output::Guest for Wasiless {
     type TerminalOutput = TerminalOutput;
 }
 
-impl terminal_stdin::Guest for MyComponent {
-    fn get_terminal_stdin() -> Option<<MyComponent as terminal_input::Guest>::TerminalInput> {
+impl terminal_stdin::Guest for Wasiless {
+    fn get_terminal_stdin() -> Option<<Wasiless as terminal_input::Guest>::TerminalInput> {
         None
     }
 }
 
-export!(MyComponent);
+export!(Wasiless);
