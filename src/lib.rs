a818fe3577f554e74286962622cb8bc6e4d95163
Erik Rose <erik.rose@fastly.com>
2025-09-26 11:21:18 -0400

    Add a non-working implementation of `terminal-input`.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,34 @@ wit_bindgen::generate!({
     path: "wit",
     generate_all,
 });
+
+// This already miraculously exports wasi::cli::terminal_input::TerminalInput!
+
+use exports::wasi::cli::terminal_input::{Guest, GuestTerminalInput, TerminalInput};
+
+static mut ONE_TRUE_TERMINAL: u8 = 0;
+
+// TODO: Make less bogus so it stands a chance of not crashing at runtime. For now, I'm just seeing if I can get it to link.
+impl GuestTerminalInput for TerminalInput {
+    unsafe fn _resource_new(val: *mut u8) -> u32
+    where
+        Self: Sized,
+    {
+        0
+    }
+
+    fn _resource_rep(handle: u32) -> *mut u8
+    where
+        Self: Sized,
+    {
+        &raw mut ONE_TRUE_TERMINAL
+    }
+}
+
+struct MyComponent;
+
+impl Guest for MyComponent {
+    type TerminalInput = TerminalInput;
+}
+
+export!(MyComponent);
