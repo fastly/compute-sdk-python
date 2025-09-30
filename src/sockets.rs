5f17fad81ce97f85c46fbf329db6db89ea5fe997
Erik Rose <erik.rose@fastly.com>
2025-09-30 17:31:06 -0400

    Add `wasi:sockets/instance-network`.

--- src/sockets.rs
+++ src/sockets.rs
@@ -1,8 +1,15 @@
-use crate::Wasiless;
+use crate::bindings::wasi::sockets::instance_network::{self};
 use crate::bindings::wasi::sockets::network::{self, GuestNetwork, Network};
+use crate::{BOGUS_HANDLE, Wasiless};
 
 impl GuestNetwork for Network {}
 
 impl network::Guest for Wasiless {
     type Network = Network;
+}
+
+impl instance_network::Guest for Wasiless {
+    fn instance_network() -> Network {
+        unsafe { Network::from_handle(BOGUS_HANDLE) }
+    }
 }
