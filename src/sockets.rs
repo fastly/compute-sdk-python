ae4c0a1d6ced4314f50c122eb8b134a65e6a25da
Erik Rose <erik.rose@fastly.com>
2025-10-01 10:46:57 -0400

    Add `wasi:sockets/tcp`.

--- src/sockets.rs
+++ src/sockets.rs
@@ -3,6 +3,7 @@ use crate::bindings::wasi::sockets::instance_network;
 use crate::bindings::wasi::sockets::network::{
     self, ErrorCode, GuestNetwork, IpAddressFamily, IpSocketAddress, Network, NetworkBorrow,
 };
+use crate::bindings::wasi::sockets::tcp::{self, GuestTcpSocket, TcpSocket};
 use crate::bindings::wasi::sockets::udp::{
     self, GuestIncomingDatagramStream, GuestOutgoingDatagramStream, GuestUdpSocket,
     IncomingDatagram, IncomingDatagramStream, OutgoingDatagram, OutgoingDatagramStream, UdpSocket,
@@ -115,4 +116,132 @@ impl udp_create_socket::Guest for Wasiless {
     fn create_udp_socket(_address_family: IpAddressFamily) -> Result<UdpSocket, ErrorCode> {
         unimplemented!()
     }
+}
+
+impl GuestTcpSocket for TcpSocket {
+    fn start_bind(
+        &self,
+        _network: tcp::NetworkBorrow<'_>,
+        _local_address: tcp::IpSocketAddress,
+    ) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn finish_bind(&self) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn start_connect(
+        &self,
+        _network: tcp::NetworkBorrow<'_>,
+        _remote_address: tcp::IpSocketAddress,
+    ) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn finish_connect(&self) -> Result<(tcp::InputStream, tcp::OutputStream), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn start_listen(&self) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn finish_listen(&self) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn accept(
+        &self,
+    ) -> Result<(tcp::TcpSocket, tcp::InputStream, tcp::OutputStream), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn local_address(&self) -> Result<tcp::IpSocketAddress, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn remote_address(&self) -> Result<tcp::IpSocketAddress, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn is_listening(&self) -> bool {
+        unreachable!()
+    }
+
+    fn address_family(&self) -> tcp::IpAddressFamily {
+        unreachable!()
+    }
+
+    fn set_listen_backlog_size(&self, _value: u64) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn keep_alive_enabled(&self) -> Result<bool, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn set_keep_alive_enabled(&self, _value: bool) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn keep_alive_idle_time(&self) -> Result<tcp::Duration, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn set_keep_alive_idle_time(&self, _value: tcp::Duration) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn keep_alive_interval(&self) -> Result<tcp::Duration, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn set_keep_alive_interval(&self, _value: tcp::Duration) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn keep_alive_count(&self) -> Result<u32, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn set_keep_alive_count(&self, _value: u32) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn hop_limit(&self) -> Result<u8, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn set_hop_limit(&self, _value: u8) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn receive_buffer_size(&self) -> Result<u64, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn set_receive_buffer_size(&self, _value: u64) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn send_buffer_size(&self) -> Result<u64, tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn set_send_buffer_size(&self, _value: u64) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+
+    fn subscribe(&self) -> tcp::Pollable {
+        unreachable!()
+    }
+
+    fn shutdown(&self, _shutdown_type: tcp::ShutdownType) -> Result<(), tcp::ErrorCode> {
+        unreachable!()
+    }
+}
+
+impl tcp::Guest for Wasiless {
+    type TcpSocket = TcpSocket;
 }
